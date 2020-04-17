6ff2700afafba5700d469f7ca48d15ebeee31b1f
Varun Garg <v8garg@uwaterloo.ca>
2020-04-17 18:41:43 -0400

    Fleshing out views file, and setup templates

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+
+from.models import Pet
 
 def home(request):
-    return HttpResponse('<p>home view</p>')
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {'pets': pets})
 
 def pet_detail(request, id):
-    return HttpResponse(f'<p>pet_detail view with the id {id}</p>')
+    try:
+        pet = Pet.objects.get(id=id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet not found')
+    return render(request, 'pet_detail.html', {'pet': pet})
