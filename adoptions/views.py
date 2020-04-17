557a401f5d56af48221b67d9cbd6d7aab08a8c2d
Varun Garg <v8garg@uwaterloo.ca>
2020-04-17 18:23:57 -0400

    Implemented url patterns and their respective views

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse('<p>home view</p>')
+
+def pet_detail(request, id):
+    return HttpResponse(f'<p>pet_detail view with the id {id}</p>')
